ba667e6f97e109a419920b928472b16a7bb41ab8
Beat Ottiger <ottigerb@gmail.com>
2016-11-23 13:49:31 +0100

    Calculation :: Example

--- app/partial/home/home.js
+++ app/partial/home/home.js
@@ -3,12 +3,22 @@ angular.module('optinomicCalculation').controller('HomeCtrl', function($scope, d
 
     // --------------------------------------------------
     // inject factory :: app_factory
+    // init
     // --------------------------------------------------
     $scope.d = data;
     $scope.d.input = input;
 
     $scope.selectedTabIndex = 1;
 
+
+    $scope.d.timings = {};
+    $scope.d.timings.start = performance.now();
+    // --------------------------------------------------
+    // Run Calculation
+    // --------------------------------------------------
+    $scope.d.output = calculation.main(input);
+    $scope.d.timings.end = performance.now();
+    $scope.d.timings.duration = $scope.d.timings.end - $scope.d.timings.start;
 
     console.log('Optinoic-Calculation :: ', $scope.d);
 
